7b91f5445d6bdca10b26bdc6b7ee55ad613b6e64
Aaron Dunlap <aaronmdunlap@icloud.com>
2023-12-05 10:47:41 -0600

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,32 @@
 -- Bootstrap Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
 require("mappings")
 require("lazy").setup({
-	{ import = "plugins.core" },
-	{ import = "plugins.lsp" },
-	{ import = "plugins.debug" },
-	{ import = "plugins.linting" },
+    { import = "plugins.core" },
+    { import = "plugins.lsp" },
+    { import = "plugins.debug" },
+    { import = "plugins.linting" },
+    { import = "plugins.formatting" },
 })
 require("aaron")
 
 local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- vim.cmd.colorscheme("catppuccin")
