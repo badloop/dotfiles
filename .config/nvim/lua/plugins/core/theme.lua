55958f4e8653bb224ee3d83e90a4002100ae240e
Aaron Dunlap <aaronmdunlap@icloud.com>
2024-05-03 06:56:34 -0500

    Updates

--- .config/nvim/lua/plugins/core/theme.lua
+++ .config/nvim/lua/plugins/core/theme.lua
@@ -6,6 +6,15 @@ return {
         opts = {}
     },
     {
+        "artanikin/vim-synthwave84",
+        lazy = false,
+    },
+    {
+        "challenger-deep-theme/vim",
+        name = "challenger-deep",
+        lazy = false,
+    },
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000, -- make sure to load this before all the other start plugins
