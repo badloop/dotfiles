3af543ef0a786515353818902466c7fab5f6e23b
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-02-07 09:05:33 -0600

    neotree config

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -1,5 +1,12 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{
+			"<leader>le",
+			"<cmd>Neotree position=current<cr>",
+			desc = "Tree file explorer",
+		},
+	},
 	opts = function(_, opts)
 		opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
 			or { "terminal", "Trouble", "qf", "Outline", "trouble" }
