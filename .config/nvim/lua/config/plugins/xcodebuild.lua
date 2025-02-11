4b294fa390d509b07bfedf1b988cd5eb72d309b2
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-02-11 08:18:00 -0600

    xcodebuild.nvim keymaps

--- .config/nvim/lua/config/plugins/xcodebuild.lua
+++ .config/nvim/lua/config/plugins/xcodebuild.lua
@@ -7,6 +7,26 @@ return {
 		"stevearc/oil.nvim", -- (optional) to manage project files
 		"nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
 	},
+	keys = {
+		{
+			"<leader>xr",
+			"<cmd>XcodebuildRun<cr>",
+			mode = { "n" },
+			desc = "XCode Run",
+		},
+		{
+			"<leader>xc",
+			"<cmd>XcodebuildCleanBuild<cr>",
+			mode = { "n" },
+			desc = "XCode Clean Build",
+		},
+		{
+			"<leader>xb",
+			"<cmd>XcodebuildBuild<cr>",
+			mode = { "n" },
+			desc = "XCode Build",
+		},
+	},
 	config = function()
 		require("xcodebuild").setup({
 			-- put some options here or leave it empty to use default settings
