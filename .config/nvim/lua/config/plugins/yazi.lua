5cdada5c50d8193c9061adf39f5c358dce0b8a43
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-02-18 12:04:28 -0600

    add yazi/tokyonight theme

---
+++ .config/nvim/lua/config/plugins/yazi.lua
@@ -0,0 +1,31 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	keys = {
+		-- 👇 in this section, choose your own keymappings!
+		{
+			"<leader>lf",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>le",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"<c-up>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	opts = {
+		-- if you want to open yazi instead of netrw, see below for more info
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+}
