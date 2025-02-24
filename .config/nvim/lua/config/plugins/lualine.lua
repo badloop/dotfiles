6ba26301290d2f3f6d303811135e27f8471a4894
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-02-24 08:51:09 -0600

    XCodebuild integrations

--- .config/nvim/lua/config/plugins/lualine.lua
+++ .config/nvim/lua/config/plugins/lualine.lua
@@ -1,55 +1,82 @@
+local function xcodebuild_device()
+	if vim.g.xcodebuild_platform == "macOS" then
+		return " macOS"
+	end
+
+	local deviceIcon = ""
+	if vim.g.xcodebuild_platform:match("watch") then
+		deviceIcon = "􀟤"
+	elseif vim.g.xcodebuild_platform:match("tv") then
+		deviceIcon = "􀡴 "
+	elseif vim.g.xcodebuild_platform:match("vision") then
+		deviceIcon = "􁎖 "
+	end
+
+	if vim.g.xcodebuild_os then
+		return deviceIcon .. " " .. vim.g.xcodebuild_device_name .. " (" .. vim.g.xcodebuild_os .. ")"
+	end
+
+	return deviceIcon .. " " .. vim.g.xcodebuild_device_name
+end
 
 return {
-    "nvim-lualine/lualine.nvim",
-    enabled = true,
-    event = "VeryLazy",
-    init = function()
-        vim.g.lualine_laststatus = vim.o.laststatus
-        if vim.fn.argc(-1) > 0 then
-            -- set an empty statusline till lualine loads
-            vim.o.statusline = " "
-        else
-            -- hide the statusline on the starter page
-            vim.o.laststatus = 0
-        end
-    end,
-    opts = {
-        theme = "auto",
-        globalstatus = true,
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = {
-                {
-                    "branch",
-                    icon = { "" },
-                },
-                {
-                    "diagnostics",
-                    sources = { "nvim_diagnostic", "nvim_lsp" },
-                    sections = { "error", "warn", "info", "hint" },
-                    diagnostics_color = {
-                        -- Same values as the general color option can be used here.
-                        error = "DiagnosticError", -- Changes diagnostics' error color.
-                        warn = "DiagnosticWarn",   -- Changes diagnostics' warn color.
-                        info = "DiagnosticInfo",   -- Changes diagnostics' info color.
-                        hint = "DiagnosticHint",   -- Changes diagnostics' hint color.
-                    },
-                    symbols = { error = " ", warn = " ", info = "󰌶 ", hint = " " },
+	"nvim-lualine/lualine.nvim",
+	enabled = true,
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			-- hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
+	opts = {
+		theme = "auto",
+		globalstatus = true,
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = {
+				{
+					"branch",
+					icon = { "" },
+				},
+				{
+					"diagnostics",
+					sources = { "nvim_diagnostic", "nvim_lsp" },
+					sections = { "error", "warn", "info", "hint" },
+					diagnostics_color = {
+						-- Same values as the general color option can be used here.
+						error = "DiagnosticError", -- Changes diagnostics' error color.
+						warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+						info = "DiagnosticInfo", -- Changes diagnostics' info color.
+						hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+					},
+					symbols = { error = " ", warn = " ", info = "󰌶 ", hint = " " },
 
-                    update_in_insert = false, -- Update diagnostics in insert mode.
-                    always_visible = false,   -- Show diagnostics even if there are none.
-                },
-            },
-            lualine_c = {
-                {
-                    "filename",
-                    path = 2,
-                    file_status = true,
-                },
-            },
-            lualine_x = { "filetype", "encoding", "tabs" },
-            lualine_y = { "location" },
-            lualine_z = {},
-        },
-    },
+					update_in_insert = false, -- Update diagnostics in insert mode.
+					always_visible = false, -- Show diagnostics even if there are none.
+				},
+			},
+			lualine_c = {
+				{
+					"filename",
+					path = 2,
+					file_status = true,
+				},
+			},
+			lualine_x = {
+				{ "' ' .. vim.g.xcodebuild_last_status", color = { fg = "Gray" } },
+				{ "'󰙨 ' .. vim.g.xcodebuild_test_plan", color = { fg = "#a6e3a1", bg = "#161622" } },
+				{ xcodebuild_device, color = { fg = "#f9e2af", bg = "#161622" } },
+				"filetype",
+				"encoding",
+				"tabs",
+			},
+			lualine_y = { "location" },
+			lualine_z = {},
+		},
+	},
 }
--- .config/nvim/lua/config/plugins/xcodebuild.lua
+++ .config/nvim/lua/config/plugins/xcodebuild.lua
@@ -40,8 +40,39 @@ return {
 		},
 	},
 	config = function()
+		local progress_handle
 		require("xcodebuild").setup({
-			-- put some options here or leave it empty to use default settings
+			show_build_progress_bar = false,
+			logs = {
+				notify = function(message, severity)
+					local fidget = require("fidget")
+					if progress_handle then
+						progress_handle.message = message
+						if not message:find("Loading") then
+							progress_handle:finish()
+							progress_handle = nil
+							if vim.trim(message) ~= "" then
+								fidget.notify(message, severity)
+							end
+						end
+					else
+						fidget.notify(message, severity)
+					end
+				end,
+				notify_progress = function(message)
+					local progress = require("fidget.progress")
+
+					if progress_handle then
+						progress_handle.title = ""
+						progress_handle.message = message
+					else
+						progress_handle = progress.handle.create({
+							message = message,
+							lsp_client = { name = "xcodebuild.nvim" },
+						})
+					end
+				end,
+			},
 		})
 	end,
 }
