0c99cc7c2d51535496b834ce878dfa69f21f4f41
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-02-24 09:30:12 -0600

    Add snacks proper

---
+++ .config/nvim/lua/config/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		image = { enabled = true },
+		indent = { enabled = true, opts = { animate = { enabled = false } } },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+	},
+}
