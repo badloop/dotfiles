edf2293a2cb455748e929f469af0615ade5c76b5
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-05-30 16:02:34 -0500

    gitsigns config

--- .config/nvim/lua/config/plugins/gitsigns.lua
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -1,20 +1,28 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	lazy = false,
-	opts = {},
-	keys = {
-		{
-			"<leader>gp",
-			"<cmd>Gitsigns preview_hunk<cr>",
-			mode = { "n" },
-			desc = "Git Preview Hunk",
-		},
-		{
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    opts = {
+        preview_config = {
+            border = "single",
+            style = "minimal",
+            relative = "cursor",
+            row = 0,
+            col = 1,
+        },
+    },
+    keys = {
+        {
+            "<leader>gp",
+            "<cmd>Gitsigns preview_hunk<cr>",
+            mode = { "n" },
+            desc = "Git Preview Hunk",
+        },
+        {
 
-			"<leader>gr",
-			"<cmd>Gitsigns reset_hunk<cr>",
-			mode = { "n" },
-			desc = "Git Reset Hunk",
-		},
-	},
+            "<leader>gr",
+            "<cmd>Gitsigns reset_hunk<cr>",
+            mode = { "n" },
+            desc = "Git Reset Hunk",
+        },
+    },
 }
