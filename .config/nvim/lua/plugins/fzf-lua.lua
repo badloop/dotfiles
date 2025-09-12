ea9223817bcb1e30afb7997e85d3b43fbf785816
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-09-11 21:34:40 -0500

    stuff

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,6 +1,8 @@
 return {
     "ibhagwan/fzf-lua",
     opts = {
+        "telescope",
+        "fzf-native",
         fzf_opts = {
             ["--layout"] = "default",
             ["--exact"] = "",
