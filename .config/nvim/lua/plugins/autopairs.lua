f17874ac3457ebf6761b02ee06cbbd4ca8108f05
Aaron Dunlap <aaronmdunlap@icloud.com>
2025-09-15 14:54:15 -0500

    tmux

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -2,4 +2,19 @@ return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true,
+    opts = {
+        check_ts = true,
+        ts_config = { java = false },
+        fast_wrap = {
+            map = "<M-e>",
+            chars = { "{", "[", "(", '"', "'" },
+            pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+            offset = 0,
+            end_key = "$",
+            keys = "qwertyuiopzxcvbnmasdfghjkl",
+            check_comma = true,
+            highlight = "PmenuSel",
+            highlight_grey = "LineNr",
+        },
+    },
 }
